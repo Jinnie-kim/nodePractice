d7dfc3f072cd9c6cc69edfa93f9cc7b79f5078be
Jinnie <hjk7639@gmail.com>
2023-03-31 23:57:50 +0900

    express, parmas로 GET 요청

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/user/:id', (req, res) => {
+  const q = req.params;
+  console.log(q.id);
+
+  res.json({ userid: q.id });
+});
+
 app.get('/dog', (req, res) => {
   res.send({ sound: '멍멍' });
   // res.json({ sound: '멍멍' }); json 데이터를 보낸다고 명시적으로 표시할 수도 있음

6ebb1bb59554c05e3707dabe4f1c427c6b87dd90
Jinnie <hjk7639@gmail.com>
2023-03-31 23:44:38 +0900

    express 실습, 간단한 api 만들어보기

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var figlet = require('figlet');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-figlet('Hello World!!', function (err, data) {
-  if (err) {
-    console.log('Something went wrong...');
-    console.dir(err);
-    return;
-  }
-  console.log(data);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/dog', (req, res) => {
+  res.send({ sound: '멍멍' });
+  // res.json({ sound: '멍멍' }); json 데이터를 보낸다고 명시적으로 표시할 수도 있음
+});
+
+app.get('/cat', (req, res) => {
+  res.send({ sound: '야옹' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });

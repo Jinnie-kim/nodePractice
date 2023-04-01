32998387dad65ce2b9aa846ba4d098467870b5d8
Jinnie <hjk7639@gmail.com>
2023-04-01 09:30:32 +0900

    express, 동물 소리 api 만들기 실습

--- index.js
+++ index.js
@@ -23,13 +23,26 @@ app.get('/user/:id', (req, res) => {
   res.json({ userid: q.name });
 });
 
-app.get('/dog', (req, res) => {
-  res.send({ sound: '멍멍' });
-  // res.json({ sound: '멍멍' }); json 데이터를 보낸다고 명시적으로 표시할 수도 있음
-});
+// app.get('/dog', (req, res) => {
+//   res.send({ sound: '멍멍' });
+//   // res.json({ sound: '멍멍' }); json 데이터를 보낸다고 명시적으로 표시할 수도 있음
+// });
 
-app.get('/cat', (req, res) => {
-  res.send({ sound: '야옹' });
+// app.get('/cat', (req, res) => {
+//   res.send({ sound: '야옹' });
+// });
+
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params;
+  if (name === 'dog') {
+    res.json({ sound: '멍멍' });
+  } else if (name === 'cat') {
+    res.json({ sound: '야옹' });
+  } else if (name === 'pig') {
+    res.json({ sound: '꿀꿀' });
+  } else {
+    res.json({ sound: '알수없음' });
+  }
 });
 
 app.listen(port, () => {
